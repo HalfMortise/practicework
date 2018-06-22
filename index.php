7b88c9413d486a72fb8f5cff4cb1f83ebb991b6c
Half Mortise <40005610+HalfMortise@users.noreply.github.com>
2018-06-22 17:22:32 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<title>Ongoing Practice</title>
+	</head>
+	<body>
+		<p>Ongoing practice in preparation for bootcamp</p>
+	</body>
+</html>
